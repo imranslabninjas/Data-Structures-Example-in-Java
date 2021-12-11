c3350e41829e76e25e29a48842adfc8685619dcd
Islam, Imranul <imranul.islam@oracle.com>
2021-12-11 16:49:54 -0500

    Removed unnecessary stars from comment statement

--- src/com/imranslab/courses/datastructure/leetcodeexamples/AddTwoNumbers.java
+++ src/com/imranslab/courses/datastructure/leetcodeexamples/AddTwoNumbers.java
@@ -1,10 +1,10 @@
 package com.imranslab.courses.datastructure.leetcodeexamples;
 
 /*
-* You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order,
-* and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order,
+and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
 You may assume the two numbers do not contain any leading zero, except the number 0 itself.
-* https://leetcode.com/problems/add-two-numbers/
+https://leetcode.com/problems/add-two-numbers/
 * */
 
 import java.util.LinkedList;
