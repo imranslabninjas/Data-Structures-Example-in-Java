6a48372e8eea8404b3c260c5e8b1555d22737134
Islam, Imranul <imranul.islam@oracle.com>
2021-12-11 12:11:50 -0500

    Removed Comments from HashMapExample class

--- src/com/company/HashMapExample.java
+++ src/com/company/HashMapExample.java
@@ -3,18 +3,12 @@ package com.company;// Java program to illustrate HashMap class of java.util
 // Importing HashMap class
 import java.util.HashMap;
 
-// Main class
 public class HashMapExample {
 
-    // Main driver method
     public static void main(String[] args)
     {
-        // Create an empty hash map by declaring object
-        // of string and integer type
         HashMap<String, Integer> map = new HashMap<>();
 
-        // Adding elements to the Map
-        // using standard add() method
         map.put("Mercury", 100);
         map.put("Venus", 200);
         map.put("Earth", 300);
@@ -24,22 +18,15 @@ public class HashMapExample {
         map.put("Neptune", 700);
         map.put("Pluto", 800);
 
-        // Print size and content of the Map
         System.out.println("Size of map is:- "
                 + map.size());
 
-        // Printing elements in object of Map
         System.out.println(map);
 
-        // Checking if a key is present and if
-        // present, print value by passing
-        // random element
         if (map.containsKey("Earth")) {
 
-            // Mapping
             Integer a = map.get("Earth");
 
-            // Printing value fr the corresponding key
             System.out.println("value for key"
                     + " \"Earth\" is:" + a);
         }
