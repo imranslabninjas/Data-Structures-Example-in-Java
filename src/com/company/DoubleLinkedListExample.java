6f152917c8e1638fb6e2c770ea1e1a26ff59479c
Islam, Imranul <imranul.islam@oracle.com>
2021-12-11 08:13:39 -0500

    Double Linked List Example - need to add more details

--- src/com/company/DoubleLinkedListExample.java
+++ src/com/company/DoubleLinkedListExample.java
@@ -1,9 +1,22 @@
 package com.company;
 
+import java.util.LinkedList;
+
 public class DoubleLinkedListExample {
 
     public static void main(String[] args) {
         // Test new branch
-        // Commit Test
+        LinkedList<String> doubleLinkedListExample = new LinkedList<>();
+
+        doubleLinkedListExample.add("Mercury");
+        doubleLinkedListExample.add("Venus");
+        doubleLinkedListExample.add("Earth");
+        doubleLinkedListExample.add("Mars");
+        doubleLinkedListExample.add("Jupiter");
+        doubleLinkedListExample.add("Saturn");
+        doubleLinkedListExample.add("Neptune");
+        doubleLinkedListExample.add("Pluto");
+
+        System.out.println(doubleLinkedListExample);
     }
 }
