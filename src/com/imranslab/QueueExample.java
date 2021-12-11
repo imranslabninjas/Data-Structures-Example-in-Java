0ff9444917edfa9e114c25a926c12cb6b896c599
Islam, Imranul <imranul.islam@oracle.com>
2021-12-11 12:33:19 -0500

    Queue Example added

--- src/com/imranslab/QueueExample.java
+++ src/com/imranslab/QueueExample.java
@@ -1,4 +1,37 @@
-package com.imranslab;
+package com.imranslab;// Java program to demonstrate a Queue
+
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class QueueExample {
+
+    public static void main(String[] args)
+    {
+        Queue<Integer> queue = new LinkedList<>();
+
+        // Adds elements {0, 1, 2, 3, 4} to
+        // the queue
+        for (int i = 0; i < 5; i++)
+            queue.add(i);
+
+        // Display contents of the queue.
+        System.out.println("Elements of queue " + queue);
+
+        // To remove the head of queue.
+        int removedElement = queue.remove();
+        System.out.println("removed element" + removedElement);
+
+        System.out.println(queue);
+
+        // To view the head of queue
+        int head = queue.peek();
+        System.out.println("head of queue " + head);
+
+        // Rest all methods of collection
+        // interface like size and contains
+        // can be used with this
+        // implementation.
+        int size = queue.size();
+        System.out.println("Size of queue " + size);
+    }
 }
